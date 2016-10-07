36b96d36343fa4a469df1825e92745e403a0c535
GM Culp <GMCulp@gmail.com>
2016-10-07 13:03:36 -0400

    Update GBAT_df.R

--- R/GBAT_df.R
+++ R/GBAT_df.R
@@ -7,6 +7,20 @@ GBAT_df <- function(x, id_col, add_col, third_col, third_col_type) {
     out_df[[paste0(as.character(GC_output$GC_function[i]),"_",as.character(GC_output$GC_colname[i]))]] <<- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", substr(out_df[[paste0(as.character(GC_output$GC_function[i]),"_output")]], GC_output$GC_start[i], GC_output$GC_end[i]), perl=TRUE)
   )
 
+  #add columns for census geographies formatted according to USCB
+  out_df[['F1E_USCBStateCounty']] <- ifelse(out_df[['F1E_CensusBoro']]==1,'36061',
+                                            ifelse(out_df[['F1E_CensusBoro']]==2,'36005',
+                                                   ifelse(out_df[['F1E_CensusBoro']]==3,'36047',
+                                                          ifelse(out_df[['F1E_CensusBoro']]==4,'36081',
+                                                                 ifelse(out_df[['F1E_CensusBoro']]==5,'36085',"")))))
+
+  out_df[['F1E_1990_USCBTract']] <- ifelse(out_df[['F1E_USCBStateCounty']]!="", paste0(out_df[['F1E_USCBStateCounty']], sprintf("%06d", as.numeric(out_df[['F1E_1990CensusTract']]))),"")
+  out_df[['F1E_2000_USCBTract']] <- ifelse(out_df[['F1E_USCBStateCounty']]!="", paste0(out_df[['F1E_USCBStateCounty']], sprintf("%06d", as.numeric(out_df[['F1E_2000CensusTract']]))),"")
+  out_df[['F1E_2010_USCBTract']] <- ifelse(out_df[['F1E_USCBStateCounty']]!="", paste0(out_df[['F1E_USCBStateCounty']], sprintf("%06d", as.numeric(out_df[['F1E_2010CensusTract']]))),"")
+
+  out_df[['F1E_2000_USCBBlock']] <- ifelse(out_df[['F1E_USCBStateCounty']]!="", paste0(out_df[['F1E_2000_USCBTract']], sprintf("%04d", as.numeric(out_df[['F1E_2000CensusBlock']])), out_df[['F1E_2000CensusBlockSuffix']]),"")
+  out_df[['F1E_2010_USCBBlock']] <- ifelse(out_df[['F1E_USCBStateCounty']]!="", paste0(out_df[['F1E_2010_USCBTract']], sprintf("%04d", as.numeric(out_df[['F1E_2010CensusBlock']])), out_df[['F1E_2010CensusBlockSuffix']]),"")
+
   #remove space deliminated output
   out_df$F1A_output <- NULL
   out_df$F1E_output <- NULL
