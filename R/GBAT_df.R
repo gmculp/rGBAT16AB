ca93d87766ee609bba853926952bf8d258e80904
GM Culp <GMCulp@gmail.com>
2017-02-13 14:05:21 -0500

    Update GBAT_df.R

--- R/GBAT_df.R
+++ R/GBAT_df.R
@@ -4,8 +4,12 @@ GBAT_df <- function(x, id_col, add_col, third_col, third_col_type) {
 
   #break output into fixed width columns
   sapply(1:nrow(GC_output), function(i)
-    out_df[[paste0(as.character(GC_output$GC_function[i]),"_",as.character(GC_output$GC_colname[i]))]] <<- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", substr(out_df[[paste0(as.character(GC_output$GC_function[i]),"_output")]], GC_output$GC_start[i], GC_output$GC_end[i]), perl=TRUE)
-  )
+    if(as.character(GC_output$GC_colname[i]) %in% c('1990CensusTract','2000CensusTract','2010CensusTract','2010CensusBlock','2000CensusBlock')){
+      out_df[[paste0(as.character(GC_output$GC_function[i]),"_",as.character(GC_output$GC_colname[i]))]] <<- gsub("\\s","0", substr(out_df[[paste0(as.character(GC_output$GC_function[i]),"_output")]], GC_output$GC_start[i], GC_output$GC_end[i]), perl=TRUE)
+    } else{
+      out_df[[paste0(as.character(GC_output$GC_function[i]),"_",as.character(GC_output$GC_colname[i]))]] <<- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", substr(out_df[[paste0(as.character(GC_output$GC_function[i]),"_output")]], GC_output$GC_start[i], GC_output$GC_end[i]), perl=TRUE)
+    }
+    )
 
   #remove space deliminated output
   out_df$F1A_output <- NULL
@@ -26,13 +30,13 @@ GBAT_census_format <- function(x) {
                                                                  ifelse(x[['F1E_CensusBoro']]==5,'36085','00000')))))
 
 
+  x[['F1E_1990_USCBTract']] <- paste0(as.character(x[['F1E_USCBStateCounty']]),as.character(x[['F1E_1990CensusTract']]))
+  x[['F1E_2000_USCBTract']] <- paste0(as.character(x[['F1E_USCBStateCounty']]),as.character(x[['F1E_2000CensusTract']]))
+  x[['F1E_2010_USCBTract']] <- paste0(as.character(x[['F1E_USCBStateCounty']]),as.character(x[['F1E_2010CensusTract']]))
 
-  x[['F1E_1990_USCBTract']] <- gsub("^0.*$","",paste0(as.character(x[['F1E_USCBStateCounty']]), sprintf("%06d", as.numeric(paste0('0', gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", x[['F1E_1990CensusTract']], perl=TRUE))))))
-  x[['F1E_2000_USCBTract']] <- gsub("^0.*$","",paste0(as.character(x[['F1E_USCBStateCounty']]), sprintf("%06d", as.numeric(paste0('0', gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", x[['F1E_2000CensusTract']], perl=TRUE))))))
-  x[['F1E_2010_USCBTract']] <- gsub("^0.*$","",paste0(as.character(x[['F1E_USCBStateCounty']]), sprintf("%06d", as.numeric(paste0('0', gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", x[['F1E_2010CensusTract']], perl=TRUE))))))
+  x[['F1E_2000_USCBBlock']] <- paste0(as.character(x[['F1E_2000_USCBTract']]),as.character(x[['F1E_2000CensusBlock']]),as.character(x[['F1E_2000CensusBlockSuffix']]))
+  x[['F1E_2010_USCBBlock']] <- paste0(as.character(x[['F1E_2010_USCBTract']]),as.character(x[['F1E_2010CensusBlock']]),as.character(x[['F1E_2010CensusBlockSuffix']]))
 
-  x[['F1E_2000_USCBBlock']] <- gsub("^0.*$","",paste0(as.character(x[['F1E_2000_USCBTract']]), sprintf("%04d", as.numeric(paste0('0', gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", x[['F1E_2000CensusBlock']], perl=TRUE)))),x[['F1E_2000CensusBlockSuffix']]))
-  x[['F1E_2010_USCBBlock']] <- gsub("^0.*$","",paste0(as.character(x[['F1E_2010_USCBTract']]), sprintf("%04d", as.numeric(paste0('0', gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", x[['F1E_2010CensusBlock']], perl=TRUE)))),x[['F1E_2010CensusBlockSuffix']]))
+
   return(x)
 }
-
